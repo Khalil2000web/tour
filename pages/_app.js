6c8015dca7529ce77e1dd1db308d3ad15d3fb380
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-02 17:08:04 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import '../styles/main.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header className="navbar">
+        <div className="nav-links">
+          <a href="https://khaliil.com/">Home</a>
+          <a href="https://tour.khaliil.com/">Tour</a>
+          <a id="nbold">K//</a>
+        </div>
+      </header>
+
+      <Component {...pageProps} />
+
+      <footer className="footer">
+        <div className="footer-links">
+          <p>&copy; 2025 Khaliil</p>
+          <a href="https://khaliil.com/legal">Legal</a>
+        </div>
+      </footer>
+    </>
+  )
 }
