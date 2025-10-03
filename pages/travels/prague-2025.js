ffa8019f77e8c478c536dc68f067e6d2f41794f8
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-03 18:33:19 +0300

    Create prague-2025.js

---
+++ pages/travels/prague-2025.js
@@ -0,0 +1,74 @@
+import TravelsLayout from './TravelsLayout.js';
+import Head from 'next/head';
+
+export default function Parge2025Page() {
+  return (
+    <TravelsLayout>
+      <Head>
+        <title>PARGE 2025</title>
+        <meta name="description" content="Khaliil's Official Website" />
+        <meta name="url" content="https://www.khaliil.com" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Khaliil" />
+        <meta name="twitter:description" content="Khaliil's Official Website" />
+        <meta name="twitter:image" content="/static/images/19311D40-948D-475E-A39B-9FB6AC451657.jpeg" />
+        <meta property="og:title" content="Khaliil" />
+        <meta property="og:description" content="Khaliil's Official Website" />
+        <meta property="og:image" content="/static/images/19311D40-948D-475E-A39B-9FB6AC451657.jpeg" />
+        <meta property="og:site_name" content="Khaliil" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.khaliil.com/" />
+        <meta httpEquiv="Content-type" content="text/html; charset=utf-8" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png" />
+        <meta name="copyright" content="2025" />
+      </Head>
+      
+      <div className="travel-title-overlay">
+  <h1>PARGE 2025</h1>
+</div>
+      
+      <div className="galmain3">
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_3682.jpeg" className="image" alt="Image 1" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_2491.jpeg" className="image" alt="Image 2" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_3665.jpeg" className="image" alt="Image 3" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_1597.jpeg" className="image" alt="Image 4" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_3692.jpeg" className="image" alt="Image 5" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_3672.jpeg" className="image" alt="Image 6" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_2617.jpeg" className="image" alt="Image 7" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_4747.jpeg" className="image" alt="Image 8" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_4966.jpeg" className="image" alt="Image 9" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_2493.jpeg" className="image" alt="Image 10" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_3685.jpeg" className="image" alt="Image 11" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_2612.jpeg" className="image" alt="Image 12" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_4813.jpeg" className="image" alt="Image 13" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_3674.jpeg" className="image" alt="Image 14" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_4962.jpeg" className="image" alt="Image 15" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_2924.jpeg" className="image" alt="Image 16" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_4924.jpeg" className="image" alt="Image 17" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_3669.jpeg" className="image" alt="Image 18" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_3680.jpeg" className="image" alt="Image 19" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_2557.jpeg" className="image" alt="Image 20" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_2507.jpeg" className="image" alt="Image 21" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_4919.jpeg" className="image" alt="Image 22" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_4962.jpeg" className="image" alt="Image 23" loading="lazy" decoding="async" /></div>
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://ik.imagekit.io/ntsnw5fxa/MVI_2540.mp4" muted loop autoPlay playsInline preload="auto" style={{ display: 'none' }}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_3672.jpeg" className="image" alt="Image 24" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_1603.jpeg" className="image" alt="Image 25" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_2625.jpeg" className="image" alt="Image 26" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_4926.jpeg" className="image" alt="Image 27" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-1f29174c420746d4bea24ccbcc9e105d.r2.dev/IMG_2473.jpeg" className="image" alt="Image 28" loading="lazy" decoding="async" /></div>
+      </div>
+    </TravelsLayout>
+  );
+}
