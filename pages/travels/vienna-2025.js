47948da5d147fb23a45c68bba4d151d33ab54a8d
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-03 17:44:34 +0300

    Create vienna-2025.js

---
+++ pages/travels/vienna-2025.js
@@ -0,0 +1,40 @@
+import TravelsLayout from './TravelsLayout.js';
+
+export default function Vienna2025Page() {
+  return (
+    <TravelsLayout>
+      <div className="galmain3">
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1655.jpeg" className="image" alt="Image 1" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1742.jpeg" className="image" alt="Image 2" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1621.jpeg" className="image" alt="Image 3" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1733.jpeg" className="image" alt="Image 4" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1728.jpeg" className="image" alt="Image 5" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_3985.jpeg" className="image" alt="Image 6" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1750.jpeg" className="image" alt="Image 7" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1715.jpeg" className="image" alt="Image 8" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1686.jpeg" className="image" alt="Image 9" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1743.jpeg" className="image" alt="Image 10" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1807.jpeg" className="image" alt="Image 11" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1618.jpeg" className="image" alt="Image 12" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1674.jpeg" className="image" alt="Image 13" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1688.jpeg" className="image" alt="Image 14" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_3811.jpeg" className="image" alt="Image 15" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1620.jpeg" className="image" alt="Image 16" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1731.jpeg" className="image" alt="Image 17" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1634.jpeg" className="image" alt="Image 18" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1687.jpeg" className="image" alt="Image 19" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1736.jpeg" className="image" alt="Image 20" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1753.jpeg" className="image" alt="Image 21" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1720.jpeg" className="image" alt="Image 22" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1619.jpeg" className="image" alt="Image 23" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_3815.jpeg" className="image" alt="Image 24" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1804.jpeg" className="image" alt="Image 25" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1657.jpeg" className="image" alt="Image 26" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_3975.jpeg" className="image" alt="Image 27" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_3814.jpeg" className="image" alt="Image 28" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1647.jpeg" className="image" alt="Image 29" loading="lazy" decoding="async" /></div>
+        <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1719.jpeg" className="image" alt="Image 30" loading="lazy" decoding="async" /></div>
+      </div>
+    </TravelsLayout>
+  );
+}
