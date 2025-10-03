d6f8a37606e40cd9810946cf2496b6508b3d23c5
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-03 17:53:25 +0300

    Update vienna-2025.js

--- pages/travels/vienna-2025.js
+++ pages/travels/vienna-2025.js
@@ -3,6 +3,27 @@ import TravelsLayout from './TravelsLayout.js';
 export default function Vienna2025Page() {
   return (
     <TravelsLayout>
+      <Head>
+        <title>VIENNA 2025</title>
+        <meta name="description" content="Khaliil's Official Website" />
+        <meta name="url" content="https://www.khaliil.com" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Khaliil" />
+        <meta name="twitter:description" content="Khaliil's Official Website" />
+        <meta name="twitter:image" content="/static/images/19311D40-948D-475E-A39B-9FB6AC451657.jpeg" />
+        <meta property="og:title" content="Khaliil" />
+        <meta property="og:description" content="Khaliil's Official Website" />
+        <meta property="og:image" content="/static/images/19311D40-948D-475E-A39B-9FB6AC451657.jpeg" />
+        <meta property="og:site_name" content="Khaliil" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.khaliil.com/" />
+        <meta httpEquiv="Content-type" content="text/html; charset=utf-8" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png" />
+        <meta name="copyright" content="2025" />
+      </Head>
+      
       <div className="galmain3">
         <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1655.jpeg" className="image" alt="Image 1" loading="lazy" decoding="async" /></div>
         <div className="image-wrapper"><img src="https://pub-711e690bbd0d461890cf62bf43a6282b.r2.dev/IMG_1742.jpeg" className="image" alt="Image 2" loading="lazy" decoding="async" /></div>
