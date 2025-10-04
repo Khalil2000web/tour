d1ed307c491697593e2d91ad23ba4ba6ad909745
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-04 18:56:14 +0300

    Create budapest-2025.js

---
+++ pages/budapest-2025.js
@@ -0,0 +1,153 @@
+import TravelsLayout from './TravelsLayout.js';
+import Head from 'next/head';
+
+export default function Budapest2025Page() {
+  return (
+    <TravelsLayout>
+      <Head>
+        <title>BUDAPEST 2025</title>
+        <meta name="description" content="Gallery of Budapest 2025 tour" />
+        <meta name="url" content="https://www.khaliil.com/budapest-2025" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Budapest 2025" />
+      </Head>
+      <div className="travel-title-overlay">
+  <h1>BUDAPEST 2025</h1>
+</div>
+      <div className="galmain3">
+
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2368.jpeg" className="image" alt="Image 1" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_4324.jpeg" className="image" alt="Image 2" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_4326.jpeg" className="image" alt="Image 3" loading="lazy" decoding="async" />
+        </div>
+
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://ik.imagekit.io/ntsnw5fxa/MVI_2372.mp4" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn"><img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" /></div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_4338.jpeg" className="image" alt="Image 4" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_4339.jpeg" className="image" alt="Image 5" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2139.jpeg" className="image" alt="Image 6" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2140.jpeg" className="image" alt="Image 7" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2150.jpeg" className="image" alt="Image 8" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2155.jpeg" className="image" alt="Image 9" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2164.jpeg" className="image" alt="Image 10" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2168.jpeg" className="image" alt="Image 11" loading="lazy" decoding="async" />
+        </div>
+
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://ik.imagekit.io/ntsnw5fxa/MVI_2077.mp4" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn"><img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" /></div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2169.jpeg" className="image" alt="Image 12" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2179.jpeg" className="image" alt="Image 13" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2199.jpeg" className="image" alt="Image 14" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2200.jpeg" className="image" alt="Image 15" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2227.jpeg" className="image" alt="Image 16" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2251.jpeg" className="image" alt="Image 17" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2261.jpeg" className="image" alt="Image 18" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2262.jpeg" className="image" alt="Image 19" loading="lazy" decoding="async" />
+        </div>
+
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/MVI_2079 2.mp4" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn"><img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" /></div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2263.jpeg" className="image" alt="Image 20" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2274.jpeg" className="image" alt="Image 21" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2287.jpeg" className="image" alt="Image 22" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2293.jpeg" className="image" alt="Image 23" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2297.jpeg" className="image" alt="Image 24" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2298.jpeg" className="image" alt="Image 25" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2299.jpeg" className="image" alt="Image 26" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2313.jpeg" className="image" alt="Image 27" loading="lazy" decoding="async" />
+        </div>
+
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://ik.imagekit.io/ntsnw5fxa/MVI_2080.mp4" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn"><img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" /></div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2314.jpeg" className="image" alt="Image 28" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2350.jpeg" className="image" alt="Image 29" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-62c7562398154a439829645cb8dca3d2.r2.dev/IMG_2353.jpeg" className="image" alt="Image 30" loading="lazy" decoding="async" />
+        </div>
+
+      </div>
+    </TravelsLayout>
+  );
+}
