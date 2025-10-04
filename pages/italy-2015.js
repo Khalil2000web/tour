4e9167ed4e03db00a2b1958220263c4ea563ab82
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-04 18:46:27 +0300

    Create italy-2015.js

---
+++ pages/italy-2015.js
@@ -0,0 +1,61 @@
+import TravelsLayout from './TravelsLayout.js';
+import Head from 'next/head';
+
+export default function Italy2015Page() {
+  return (
+    <TravelsLayout>
+      <Head>
+        <title>ITALY 2015</title>
+        <meta name="description" content="Gallery of Italy 2015 tour" />
+        <meta name="url" content="https://www.khaliil.com/italy-2015" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Italy 2015" />
+      </Head>
+    
+<div className="travel-title-overlay">
+  <h1>ITALY 2015</h1>
+</div>
+
+      <div className="galmain3">
+
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6296.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6297.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6298.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6299.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6300.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6301.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6302.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6303.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6304.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6305.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6306.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-7771b4a33590457daa4b7ada75458878.r2.dev/IMG_6307.jpeg" className="image" alt="Image in Italy" loading="lazy" decoding="async" />
+        </div>
+
+      </div>
+    </TravelsLayout>
+  );
+}
