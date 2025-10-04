66d80b8a3ccbeb74bfb795f3598ab65fa75aad72
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-04 18:50:34 +0300

    Create antalya-2013.js

---
+++ pages/antalya-2013.js
@@ -0,0 +1,46 @@
+import TravelsLayout from './TravelsLayout.js';
+import Head from 'next/head';
+
+export default function Antalya2013Page() {
+  return (
+    <TravelsLayout>
+      <Head>
+        <title>ANTALYA 2013</title>
+        <meta name="description" content="Gallery of Antalya 2013 tour" />
+        <meta name="url" content="https://www.khaliil.com/antalya-2013" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Antalya 2013" />
+      </Head>
+    
+      <div className="travel-title-overlay">
+  <h1>ANTALYA 2013</h1>
+</div>
+
+      <div className="galmain3">
+
+        <div className="image-wrapper">
+          <img src="https://pub-8e3e2b2b582842958a59dba84262a718.r2.dev/IMG_5691.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-8e3e2b2b582842958a59dba84262a718.r2.dev/IMG_5689.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-8e3e2b2b582842958a59dba84262a718.r2.dev/IMG_5690.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-8e3e2b2b582842958a59dba84262a718.r2.dev/IMG_5692.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-8e3e2b2b582842958a59dba84262a718.r2.dev/IMG_5693.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-8e3e2b2b582842958a59dba84262a718.r2.dev/IMG_5694.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-8e3e2b2b582842958a59dba84262a718.r2.dev/IMG_5695.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+
+      </div>
+    </TravelsLayout>
+  );
+}
