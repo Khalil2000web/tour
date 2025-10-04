67f006d9d56c76aa4e256c8458bfcc5f76556c5b
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-04 18:40:58 +0300

    Create switzerland-2024.js

---
+++ pages/switzerland-2024.js
@@ -0,0 +1,258 @@
+import TravelsLayout from './TravelsLayout.js';
+import Head from 'next/head';
+
+export default function Switzerland2024Page() {
+  return (
+    <TravelsLayout>
+      <Head>
+        <title>Switzerland 2024</title>
+        <meta name="description" content="Gallery of Switzerland 2024 tour" />
+        <meta name="url" content="https://www.khaliil.com/switzerland-2024" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Switzerland 2024" />
+      </Head>
+
+      <div className="galmain3">
+
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9238.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9274.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/Gif_10i.GIF" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://pub-19025506a9754f36baa46a24e6f84719.r2.dev/IMG_7630.mov$0" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9271.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/Title63%3F.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9263.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9262.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://github.com/Khalil2000web/Media/raw/refs/heads/main/Swis-page/IMG_9226.mov" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9261.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9260.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9259.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://pub-19025506a9754f36baa46a24e6f84719.r2.dev/IMG_6658%202.mov" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9242.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/Gif7_swis.GIF" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9245.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/IMG_9239.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://github.com/Khalil2000web/Media/raw/refs/heads/main/Swis-page/IMG_9222.mov" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+        
+<div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/IMG_9240" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/Swis_gif4.GIF" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+    
+                <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9208.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+                <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9209.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        
+                <div className="video-container">
+          <canvas></canvas>
+          <video src="https://github.com/Khalil2000web/Media/raw/refs/heads/main/Swis-page/IMG_7288.mov" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9210.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9211.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/Swis_gif66.GIF" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9212.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9213.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+                        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://github.com/Khalil2000web/Media/raw/refs/heads/main/Swis-page/IMG_9219.mov" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9214.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/Swis_gif_3.GIF" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9215.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9216.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+          <div className="video-container">
+          <canvas></canvas>
+          <video src="https://github.com/Khalil2000web/Media/raw/refs/heads/main/Swis-page/IMG_7288.mov" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/Swis_gif_2.GIF" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9218.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9220.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="video-container">
+          <canvas></canvas>
+          <video src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_7095.mov" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9221.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9223.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9224.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9225.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9227.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+                <div className="video-container">
+          <canvas></canvas>
+          <video src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/Video_2_swis_2024_tour_page_.mov" muted loop autoPlay playsInline preload="auto" style={{display: 'none'}}></video>
+          <div className="spinner"></div>
+          <div className="sound-btn">
+            <img src="https://tour.khaliil.com/static/images/icon-mute.svg" alt="Toggle Sound" />
+          </div>
+          <div className="error-message"></div>
+          <div className="play-btn"></div>
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9228.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        
+        <div className="image-wrapper">
+          <img src="https://raw.githubusercontent.com/Khalil2000web/Media/main/Swis-page/IMG_9230.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+
+      </div>
+    </TravelsLayout>
+  );
+}
