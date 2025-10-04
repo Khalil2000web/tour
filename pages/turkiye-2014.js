be6217b41ab12c3412a65bf546f610c88f220232
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-04 18:48:56 +0300

    Create turkiye-2014.js

---
+++ pages/turkiye-2014.js
@@ -0,0 +1,91 @@
+import TravelsLayout from './TravelsLayout.js';
+import Head from 'next/head';
+
+export default function Turkiye2014Page() {
+  return (
+    <TravelsLayout>
+      <Head>
+        <title>TÜRKİYE 2014</title>
+        <meta name="description" content="Gallery of Turkiye 2014 tour" />
+        <meta name="url" content="https://www.khaliil.com/turkiye-2014" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Turkiye 2014" />
+      </Head>
+    
+          <div className="travel-title-overlay">
+  <h1>TÜRKİYE 2014</h1>
+</div>
+
+      <div className="galmain3">
+
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5708.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5704.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5716.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5699.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5713.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5707.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5712.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5697.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5706.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5711.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5700.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5718.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5703.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5710.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5696.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5701.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5715.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5698.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5709.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5717.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5705.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+        <div className="image-wrapper">
+          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5714.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
+        </div>
+
+      </div>
+    </TravelsLayout>
+  );
+}
