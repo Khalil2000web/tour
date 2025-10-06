2adca7351c071c4c1bab7308d9abf6695f12c93f
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-06 20:03:47 +0300

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,5 +1,4 @@
-// 1️⃣ Import your component
-import CustomVideo from "../components/CustomVideo.js"; // adjust path if needed
+import CustomVideo from "../components/CustomVideo.js";
 
 export default function Test() {
   return (
@@ -7,8 +6,8 @@ export default function Test() {
       <h1>My Custom Video Player</h1>
       <CustomVideo
         src="/videos/IMG_7387.mp4"
-        muteIcon="/icons/mute.svg"
-        unmuteIcon="/icons/unmute.svg"
+        muteIcon="/images/volume-on.png"
+        unmuteIcon="/images/volume-off.png"
       />
     </div>
   );
