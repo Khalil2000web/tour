53f98356f8f3f0929369a77542428138668a280f
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-06 19:33:04 +0300

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,9 +1,12 @@
+// 1️⃣ Import your component
+import CustomVideo from "../components/CustomVideo.js"; // adjust path if needed
+
 export default function Test() {
   return (
     <div style={{ padding: "50px" }}>
       <h1>My Custom Video Player</h1>
       <CustomVideo
-        src="https://ik.imagekit.io/ntsnw5fxa/MVI_2540.mp"
+        src="https://ik.imagekit.io/ntsnw5fxa/MVI_2540.mp4" // make sure .mp4
         muteIcon="/icons/mute.svg"
         unmuteIcon="/icons/unmute.svg"
       />
