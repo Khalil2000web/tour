8457ad3424fa33d54d6c7d5c6173da5db274a346
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-06 19:38:21 +0300

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -6,7 +6,7 @@ export default function Test() {
     <div style={{ padding: "50px" }}>
       <h1>My Custom Video Player</h1>
       <CustomVideo
-        src="https://ik.imagekit.io/ntsnw5fxa/MVI_2540.mp4" // make sure .mp4
+        src="http://www.example.com/waterfall-video.mp4" // make sure .mp4
         muteIcon="/icons/mute.svg"
         unmuteIcon="/icons/unmute.svg"
       />
