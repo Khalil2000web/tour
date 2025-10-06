922a52d2d603fe473115862066f8ebc785bdb426
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-06 20:10:34 +0300

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -6,8 +6,8 @@ export default function Test() {
       <h1>My Custom Video Player</h1>
       <CustomVideo
         src="/videos/IMG_7387.mp4"
-        muteIcon="/images/volume-on.png"
-        unmuteIcon="/images/volume-off.png"
+        muteIcon="/icons/icon-mute.svg"
+        unmuteIcon="/icons/icon-volume.svg"
       />
     </div>
   );
