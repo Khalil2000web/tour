bb5aaaafbc2bf125c3439a14f087599c32d34672
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-06 20:27:11 +0300

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -2,9 +2,9 @@ import CustomVideo from "../components/CustomVideo.js";
 
 export default function Test() {
   return (
-    <div style={{ paddingTop: "90px" }}>
+    <div style={{ paddingTop: "150px" }}>
       <CustomVideo
-        src="/videos/IMG_7387.mp4"
+        src="/videos/IMG_7388.mp4"
         muteIcon="/icons/icon-mute.svg"
         unmuteIcon="/icons/icon-volume.svg"
       />
