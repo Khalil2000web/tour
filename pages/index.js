c33781b10462ec28d4c2086b05607dd452a141fb
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-06 09:30:55 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Home() {
   return (
+  <>
+      <Head>
+        <title>TOUR</title>
+        <meta name="description" content="Khaliil's Official Website" />
+        <meta name="url" content="https://www.khaliil.com" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Khaliil" />
+        <meta name="twitter:description" content="Khaliil's Official Website" />
+        <meta name="twitter:image" content="/static/images/19311D40-948D-475E-A39B-9FB6AC451657.jpeg" />
+        <meta property="og:title" content="Khaliil" />
+        <meta property="og:description" content="Khaliil's Official Website" />
+        <meta property="og:image" content="/static/images/19311D40-948D-475E-A39B-9FB6AC451657.jpeg" />
+        <meta property="og:site_name" content="Khaliil" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.khaliil.com/" />
+        <meta httpEquiv="Content-type" content="text/html; charset=utf-8" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png" />
+        <meta name="copyright" content="2025" />
+      </Head>
     <main className="indexmain">
       <h1 className="title-h1">Tour Archive</h1>
 
@@ -76,5 +98,6 @@ export default function Home() {
         </div>
       </section>
     </main>
+    </>
   )
 }
