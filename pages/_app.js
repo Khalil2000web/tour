6623a6dae93b13bf6094bd4b053091bc6208c9b6
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-11 18:51:32 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,13 @@
 import '../styles/globals.css'
+import MenuOverlay from '../components/MenuOverlay';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <header className="navbar">
-        <div className="nav-links">
-          <a href="https://khaliil.com/">Home</a>
-         <a href="https://khaliil.com/about">About</a>
-          <a href="/">Tour</a>
-          <a id="nbold">K//</a>
-        </div>
-      </header>
+<>
 
-      <Component {...pageProps} />
+<MenuOverlay />
+<Component {...pageProps} />
 
-      <footer className="footer">
-        <div className="footer-links">
-          <p>&copy; 2025 Khaliil</p>
-          <a href="https://khaliil.com/legal">Legal</a>
-        </div>
-      </footer>
-    </>
-  )
+</>
+);
 }
