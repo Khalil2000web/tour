40f52f95ec46b21ae358d9a3ab9a915a212b65d4
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-11-01 17:48:55 +0200

    Update turkiye-2014.js

--- pages/turkiye-2014.js
+++ pages/turkiye-2014.js
@@ -1,91 +1,108 @@
 import TravelsLayout from '../components/TravelsLayout.js';
 import Head from 'next/head';
+import CustomVideo from '../components/CustomVideo';
 
-export default function Turkiye2014Page() {
-  return (
-    <TravelsLayout>
-      <Head>
-        <title>TÜRKİYE 2014</title>
-        <meta name="description" content="Gallery of Turkiye 2014 tour" />
-        <meta name="url" content="https://www.khaliil.com/turkiye-2014" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="Turkiye 2014" />
-      </Head>
-    
-          <div className="travel-title-overlay">
-  <h1>TÜRKİYE 2014</h1>
-</div>
+export default function Turkiye2014Page({ media }) {
+return (
+<TravelsLayout>
+<Head>
+<title>TÜRKİYE 2014</title>
+<meta charSet="UTF-8" />
+<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="Khaliil's Official Website" />
+<meta name="keywords" content="Khaliil, Official Website, Portfolio, Developer, Designer" />
+<meta name="author" content="Khaliil" />
+<meta name="robots" content="index, follow" />
+<link rel="canonical" href="https://khaliil.com/" />
+
+<meta property="og:title" content="Khaliil" />
+<meta property="og:description" content="Khaliil in Türkiye 2014" />
+<meta property="og:image" content="" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:site_name" content="Khaliil" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://khaliil.com/" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:updated_time" content="2025-10-09T00:00:00+03:00" />
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="Khaliil" />
+<meta name="twitter:description" content="Khaliil in Türkiye 2014" />
+<meta name="twitter:image" content="" />
+<meta name="twitter:site" content="@khaliildiab" />
+<meta name="twitter:creator" content="@khaliildiab" />
+
+<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+<link rel="manifest" href="/manifest.json" />
+<meta name="theme-color" content="#000000" />
+
+<meta name="mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-title" content="Khaliil" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+<meta name="copyright" content="2025" />
+<meta name="referrer" content="no-referrer-when-downgrade" />
+</Head>
+
+      <div className="travel-title-overlay">
+        <h1>TÜRKİYE 2014</h1>
+      </div>
 
       <div className="galmain3">
-
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5708.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5704.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5716.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5699.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5713.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5707.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5712.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5697.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5706.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5711.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5700.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5718.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5703.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5710.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5696.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5701.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5715.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5698.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5709.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5717.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5705.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-        <div className="image-wrapper">
-          <img src="https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5714.jpeg" className="image" alt="Image" loading="lazy" decoding="async" />
-        </div>
-
+        {media.map((item, index) => (
+          <div key={index} className="image-wrapper">
+            {item.type === 'image' ? (
+              <img
+                src={item.src}
+                className="image"
+                alt={item.alt}
+                loading="lazy"
+                decoding="async"
+              />
+            ) : (
+              <CustomVideo
+                src={item.src}
+                muteIcon={item.muteIcon || '/icons/icon-mute.svg'}
+                unmuteIcon={item.unmuteIcon || '/icons/icon-volume.svg'}
+              />
+            )}
+          </div>
+        ))}
       </div>
     </TravelsLayout>
   );
+}
+
+export async function getStaticProps() {
+  const media = [
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5708.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5704.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5716.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5699.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5713.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5707.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5712.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5697.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5706.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5711.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5700.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5718.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5703.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5710.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5696.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5701.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5715.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5698.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5709.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5717.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5705.jpeg' },
+    { type: 'image', src: 'https://pub-1d34e12f60c64f108921e1253a049c38.r2.dev/IMG_5714.jpeg' },
+    // { type: 'video', src: 'https://ik.imagekit.io/cx6yav04a/91AAD0-2855-41E9-AFBC-BF7F0B030D1B-2.mp4', muteIcon: '/icons/icon-mute.svg', unmuteIcon: '/icons/icon-volume.svg', videoLink: 'https://tour.khaliil.com/' }
+  ];
+
+  return { props: { media } };
 }
